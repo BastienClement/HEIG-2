4afc204ece54784502394491cfaea2512ae06c15
galedric <g@ledric.me>
2015-11-11 17:32:08 +0100

    Hanoi

---
+++ POO/Labo7/src/hanoi/Hanoi.java
@@ -0,0 +1,128 @@
+package hanoi;
+
+import hanoi.gui.JHanoi;
+import util.Stack;
+
+import java.util.Iterator;
+
+/**
+ * Résolution du problème des tours de Hanoi
+ */
+public class Hanoi {
+    @SuppressWarnings("unchecked")
+    Stack<Integer>[] sticks = new Stack[3];
+
+    int turn = 1;
+    boolean finished = false;
+
+    public Hanoi(int h) {
+        // Construction des piquets
+        for (int i = 0; i < 3; i++) {
+            sticks[i] = new Stack<Integer>();
+        }
+
+        // Empilement des disques sur le premier piquet
+        for (; h > 0; h--) {
+            sticks[0].push(h);
+        }
+    }
+
+    /**
+     * Initialise la résolution du problème.
+     */
+    public void solve(JHanoi frame) {
+        if (finished) return;
+
+        printStep();
+        if (frame != null) frame.display();
+
+        resolve(frame, sticks[0].size(), sticks[0], sticks[2], sticks[1]);
+        finished = true;
+    }
+
+    /**
+     * Résolution récursive du problème des tours de Hanoi
+     */
+    private void resolve(JHanoi frame, int n, Stack<Integer> D, Stack<Integer> A, Stack<Integer> I) {
+        if (n == 0) return;
+
+        resolve(frame, n - 1, D, I, A);
+
+        A.push(D.pop());
+        printStep();
+        if (frame != null) frame.display();
+
+        resolve(frame, n - 1, I, A, D);
+    }
+
+    /**
+     * Affiche l'étape actuelle de la résolution sous
+     * forme textuelle.
+     */
+    private void printStep() {
+        System.out.println("-- Turn: " + turn++);
+        System.out.println(this);
+    }
+
+    /**
+     * Retourne une représentation textuelle de l'état courrant
+     * des différents piquets.
+     */
+    public String toString() {
+        String s = "One:\t" + sticks[0] + "\n";
+        s += "Two:\t" + sticks[1] + "\n";
+        s += "Three:\t" + sticks[2] + "\n";
+
+        return s;
+    }
+
+    /**
+     * Retourne une représentation sous forme de tableau des
+     * différents piquets.
+     * Note: il n'est pas possible d'utiliser la fonction Stack.toArray()
+     * puisque les types des tableaux ne sont pas les mêmes.
+     */
+    public int[][] status() {
+        int[][] t = new int[3][];
+
+        for (int i = 0; i < 3; i++) {
+            Stack<Integer> stick = sticks[i];
+            t[i] = new int[stick.size()];
+            Iterator<Integer> it = stick.iterator();
+            for (int j = 0; it.hasNext(); j++) {
+                t[i][j] = it.next();
+            }
+        }
+
+        return t;
+    }
+
+    /**
+     * Indique si la résolution du problème est terminée
+     */
+    public boolean finished() {
+        return finished;
+    }
+
+    /**
+     * Retourne le nombre de déplacement effectués
+     */
+    public int turn() {
+        return turn;
+    }
+
+    /**
+     * Fonction principale
+     */
+    public static void main(String[] args) {
+        if (args.length > 0) {
+            // Lancement en mode console si un argument est passé
+            int s = Integer.parseInt(args[0]);
+            Hanoi h = new Hanoi(s);
+            h.solve(null);
+        } else {
+            // Lancement en mode graphique
+            new JHanoi();
+        }
+    }
+}
---
+++ POO/Labo7/src/util/Stack.java
@@ -0,0 +1,136 @@
+package util;
+
+import java.util.EmptyStackException;
+import java.util.Iterator;
+
+/**
+ * Une pile.
+ */
+public class Stack<T> implements Iterable<T> {
+    /**
+     * Un élément de la liste chainée formant la pile.
+     * Note: utilisation de la visibilité publique puisque
+     * les objets de cette classe ne sont jamais exposés à
+     * l'extérieur de la classe Stack.
+     */
+    private class Element {
+        public T data;
+        public Element next;
+
+        Element(T data, Element next) {
+            this.data = data;
+            this.next = next;
+        }
+    }
+
+    /**
+     * Iterateur sur la pile
+     */
+    class StackIterator implements Iterator<T> {
+        /**
+         * L'élément courrant de l'itération
+         */
+        private Element current;
+
+        StackIterator(Element current) {
+            this.current = current;
+        }
+
+        /**
+         * Est-ce qu'il y a encore un élément auquel accéder ?
+         */
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        /**
+         * Retourne le prochain élément
+         */
+        public T next() {
+            T data = current.data;
+            current = current.next;
+            return data;
+        }
+    }
+
+    /**
+     * Taille de la pile
+     */
+    private int size = 0;
+
+    /**
+     * Sommet actuel de la pile
+     */
+    private Element top = null;
+
+    /**
+     * Ajoute un élément au sommet de la pile
+     */
+    public void push(T data) {
+        top = new Element(data, top);
+        size++;
+    }
+
+    /**
+     * Retire et retourne l'élément au sommet de la pile
+     */
+    public T pop() {
+        if (top == null) {
+            throw new EmptyStackException();
+        }
+
+        T data = top.data;
+        top = top.next;
+        size--;
+
+        return data;
+    }
+
+    /**
+     * Retourne une représentation textuelle de la pile.
+     */
+    public String toString() {
+        String s = "[";
+
+        Element l = top;
+        while (l != null) {
+            s += " <" + l.data.toString() + ">";
+            l = l.next;
+        }
+
+        s += " ]";
+        return s;
+    }
+
+    /**
+     * Transforme la pile en un tableau d'object.
+     * À cause des limitations de la généricité Java,
+     * il n'est pas possible de retourner un T[],
+     * uniquement un Object[].
+     */
+    public Object[] toArray() {
+        Object[] a = new Object[size];
+
+        Element l = top;
+        for (int i = 0; l != null; i++, l = l.next) {
+            a[i] = l.data;
+        }
+
+        return a;
+    }
+
+    /**
+     * Retourne un itérateur sur la pile
+     */
+    public Iterator<T> iterator() {
+        return new StackIterator(top);
+    }
+
+    /**
+     * Retourne la taille de la pile
+     */
+    public int size() {
+        return size;
+    }
+}
+
