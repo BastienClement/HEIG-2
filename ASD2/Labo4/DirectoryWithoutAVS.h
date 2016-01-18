fa67e176bec77f82d0a507989fb6a5f781e78913
Alain Hardy <al.hardy.16@gmail.com>
2016-01-18 13:15:18 +0100

    ASD2 : labo4

--- ASD2/Labo4/DirectoryWithoutAVS.h
+++ ASD2/Labo4/DirectoryWithoutAVS.h
@@ -3,6 +3,7 @@
 //  ASD2_2015
 //
 //  Created by Fabien Dutoit on 19.10.15.
+//  Modified by Bastien Clément & Alain Hardy on 26.10.2015
 //  Copyright © 2015 IICT, HEIG-VD. All rights reserved.
 //
 
@@ -20,7 +21,7 @@ private:
     std::string firstname;
     std::string gender;
     std::string birthday;
-    
+
 public:
     DirectoryWithoutAVS();
     DirectoryWithoutAVS(std::string avs, /* SHOULD NOT BE USED */
@@ -28,16 +29,16 @@ public:
                   std::string firstname,
                   std::string gender,
                   std::string birthday);
-    
+
     std::string getName() const;
     std::string getFirstname() const;
     std::string getGender() const;
     std::string getBirthday() const;
-    
+
     //operators
     bool operator ==(const DirectoryWithoutAVS &that) const;
     friend std::ostream& operator <<(std::ostream&, const DirectoryWithoutAVS&);
-    
+
 };
 
 namespace std
@@ -47,8 +48,12 @@ namespace std
     {
         size_t operator()(const DirectoryWithoutAVS& d) const
         {
-            //we use stl hash function for string type on name, we could do much better...
-            return std::hash<string>()(d.getName()); /* A AMELIORER */
+            size_t code = 0;
+            code = code * 23 + hash<string>()(d.getName());
+            code = code * 29 + hash<string>()(d.getFirstname());
+            code = code * 31 + hash<string>()(d.getBirthday());
+            code = code * 37 + hash<string>()(d.getGender());
+            return code;
         }
     };
 }
