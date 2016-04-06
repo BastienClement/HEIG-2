c5972c4309522980b08ed4c0a0498059f80e8ae1
Bastien Clément <g@ledric.me>
2016-04-06 20:08:40 +0200

    Formatting and double constructor

--- POO2/14 Strings/Code/ClasseString/cstring.h
+++ POO2/14 Strings/Code/ClasseString/cstring.h
@@ -6,46 +6,50 @@
  */
 
 #ifndef CSTRING_H
-#define	CSTRING_H
+#define    CSTRING_H
 
 #include <iostream>
 #include <stdexcept>
 
 class String {
 public:
-   String();
-   String(const char*);
-   String(const String&);
-   String(const char);
-   String(const int);
-   String(const bool);
-   ~String();
-   
-   const int size() const;
-   const char* c_str() const;
-   char& at(size_t) throw(std::runtime_error);
-   const char& at(size_t) const throw(std::runtime_error);
-   bool isEqual(const char*) const;
-   bool isEqual(const String&) const;
-   String& assign(const char*);
-   String& assign(const String&);
-   String& append(const char*);
-   String& append(const String&);   
-   
-   bool operator==(const char*) const;
-   bool operator==(const String&) const;
-   String& operator=(const String&);
-   String operator+(const String&) const;
-   String& operator+=(const String&);
+	String();
+	String(const char*);
+	String(const String&);
+	String(const char);
+	String(const int);
+	String(const double);
+	String(const bool);
+	String(const char*cs, size_t start, size_t len);
 
-   String& between(int, int) const throw(std::runtime_error);
-   
-   static char* to_charArray(const int);
-   
-   friend std::ostream& operator<<(std::ostream&, const String&);
+	~String();
+
+	const int size() const;
+	const char*c_str() const;
+	char&at(size_t) throw(std::runtime_error);
+	const char&at(size_t) const throw(std::runtime_error);
+	bool isEqual(const char*) const;
+	bool isEqual(const String&) const;
+	String&assign(const char*);
+	String&assign(const String&);
+	String&append(const char*);
+	String&append(const String&);
+
+	bool operator==(const char*) const;
+	bool operator==(const String&) const;
+	String&operator=(const String&);
+	String operator+(const String&) const;
+	String&operator+=(const String&);
+
+	String&between(int, int) const throw(std::runtime_error);
+
+	static char*to_charArray(const int);
+
+	friend std::ostream&operator<<(std::ostream&, const String&);
+
 private:
-   char* _str;
-   size_t _size;
+	char*_str;
+	size_t _size;
 
 };
 
